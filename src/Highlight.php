2a46358a55940f285abbbcb3acbd0c2490f32133
Stas Makarov <s.makarov@zebra-group.ru>
2015-07-12 12:48:28 +0300

    Dirty kinda working ClippingsParser

--- src/Highlight.php
+++ src/Highlight.php
@@ -8,10 +8,26 @@ class Highlight
 
     private $text;
 
-    private $bookId;
+    private $book;
 
     private $type;
 
     private $dateAdded;
+
+
+    function __construct($text, $book, $type, $dateAdded, $location)
+    {
+
+//        var_dump(func_get_args());
+
+        $this->text = $text;
+        $this->book = $book;
+        $this->type = $type;
+        $this->dateAdded = $dateAdded;
+        $this->location = $location;
+
+//        var_dump(func_get_args());
+    }
+
 
 }
